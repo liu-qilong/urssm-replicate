b3c0b64ca46357fa4ee2d14a4d2a11cdb4da6a9c
KNPOB <qilong-kirov.liu@outlook.com>
2025-07-17 18:30:25 +0800

    3dmd dataset

---
+++ src/dataset/shape_cor_3dmd.py
@@ -0,0 +1,63 @@
+from src.dataset.shape_cor_fast import ShapeDatasetFast, PairShapeDataset
+from src.infra.registry import DATASET_REGISTRY
+
+@DATASET_REGISTRY.register()
+class Single3dMDDatasetFast(ShapeDatasetFast):
+    def __init__(
+            self,
+            data_root,
+            mesh_type='ply',
+            return_faces=True,
+            return_L=False,
+            return_mass=True,
+            num_evecs=200,
+            return_evecs=True,
+            return_grad=True,
+            return_corr=False,
+            return_dist=False,
+        ):
+        # groudntruth shapr cor unavailable
+        assert not return_corr
+
+        super(Single3dMDDatasetFast, self).__init__(
+            data_root,
+            mesh_type,
+            return_faces,
+            return_L,
+            return_mass,
+            num_evecs,
+            return_evecs,
+            return_grad,
+            return_corr,
+            return_dist,
+        )
+
+
+@DATASET_REGISTRY.register()
+class Pair3dMDDataset(PairShapeDataset):
+    def __init__(
+            self,
+            data_root,
+            mesh_type='ply',
+            return_faces=True,
+            return_L=False,
+            return_mass=True,
+            num_evecs=200,
+            return_evecs=True,
+            return_grad=True,
+            return_corr=False,
+            return_dist=False,
+        ):
+        dataset = Single3dMDDatasetFast(
+            data_root,
+            mesh_type,
+            return_faces,
+            return_L,
+            return_mass,
+            num_evecs,
+            return_evecs,
+            return_grad,
+            return_corr,
+            return_dist,
+        )
+        super(Pair3dMDDataset, self).__init__(dataset)
