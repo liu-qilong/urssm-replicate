6a625788d2be04a15b61350ec85adfc6b4044fc5
KNPOB <qilong-kirov.liu@outlook.com>
2025-07-21 17:26:16 +0800

    ground-truth texture transfer sample generator metric

--- src/metric/infer_sample.py
+++ src/metric/infer_sample.py
@@ -1,15 +1,11 @@
-import time
 import shutil
 from pathlib import Path
 import pyvista as pv
-
 import torch
-import torch.distributed as dist
 
 from src.metric import BaseMetric
 from src.infra.registry import METRIC_REGISTRY
-from src.utils.fmap import fmap2pointmap_vectorized
-from src.utils.fmap import pointmap2Pyx_vectorized
+from src.utils.fmap import fmap2pointmap_vectorized, pointmap2Pyx_vectorized, corr2pointmap_vectorized
 from src.utils.tensor import to_numpy
 from src.utils.texture import write_obj_pair
 
@@ -67,6 +63,88 @@ class TextureTransferSample(BaseMetric):
                 evecs_y = data['second']['evecs'],
                 evecs_trans_x = data['first']['evecs_trans'],
                 evecs_trans_y = data['second']['evecs_trans'],
+                verts_mask_y=data['second']['verts_mask'],
+            )
+
+            name_x, name_y = data['first']['name'], data['second']['name']
+            verts_num_x = data['first']['num_verts']
+            verts_num_y = data['second']['num_verts']
+            faces_num_x = data['first']['num_faces']
+            faces_num_y = data['second']['num_faces']
+
+            for idx in range(len(infer['Cxy'])):
+                # export mesh w/ texture
+                write_obj_pair(
+                    file_name1=str(self.output_path / f'{name_x[idx]}.obj'),
+                    file_name2=str(self.output_path / f'{name_x[idx]}--{name_y[idx]}.obj'),
+                    faces1=to_numpy(data['first']['faces'][idx, :faces_num_x[idx]]),
+                    verts1=to_numpy(data['first']['verts'][idx, :verts_num_x[idx]]),
+                    verts2=to_numpy(data['second']['verts'][idx, :verts_num_y[idx]]),
+                    faces2=to_numpy(data['second']['faces'][idx, :faces_num_y[idx]]),
+                    Pyx=to_numpy(Pyx[idx, :verts_num_y[idx], :verts_num_x[idx]]),
+                    texture_file=str(self.output_path / 'texture.png'),
+                )
+
+                # render texture transfer
+                pl = pv.Plotter(off_screen=True)
+                pl.add_mesh(
+                    mesh=pv.read(self.output_path / f'{name_x[idx]}.obj'),
+                    texture=pv.read_texture(self.output_path / 'texture.png'),
+                )
+                pl.add_mesh(
+                    mesh=pv.read(self.output_path / f'{name_x[idx]}--{name_y[idx]}.obj').translate(self.shape_disp),
+                    texture=pv.read_texture(self.output_path / 'texture.png'),
+                )
+                pl.camera_position = 'xy'
+                pl.screenshot(self.output_path / f'{name_x[idx]}--{name_y[idx]}.png', window_size=self.window_size, return_img=False)
+                pl.close()
+
+                # remove the mesh files if not needed
+                if not self.keep_mesh:
+                    (self.output_path / f'{name_x[idx]}.obj').unlink(missing_ok=True)
+                    (self.output_path / f'{name_x[idx]}.mtl').unlink(missing_ok=True)
+                    (self.output_path / f'{name_x[idx]}--{name_y[idx]}.obj').unlink(missing_ok=True)
+                    (self.output_path / f'{name_x[idx]}--{name_y[idx]}.mtl').unlink(missing_ok=True)
+
+        # increment batch total
+        self.batch_total += 1
+        
+        # return zero
+        return torch.tensor(0.0).to(device=self.script.device)
+
+
+@METRIC_REGISTRY.register()
+class GTTextureTransferSample(TextureTransferSample):
+    """Generate ground-truth texture transfer samples.
+    """
+    def __init__(self, batch_interval: int = 10, texture_file: str = 'gallery/texture-symbol-grid.png', shape_disp: list = [1, 0, 0], cammera_position: str = 'xy', window_size: list = [1024, 1024], output_folder: str = 'bench/gt-texture-transfer-samples', keep_mesh: bool = True):
+        """
+        Args:
+            texture_file (str): Path to the texture file.
+            shape_disp (list): Displacement of the second shape.
+            cammera_position (str): Position of the camera.
+            batch_interval (int): Generate texture transfer samples per `batch_interval` batches.
+            output_folder (str): Folder to save the generated samples (relative to the experiment folder)
+        """
+        super().__init__(batch_interval, texture_file, shape_disp, cammera_position, window_size, output_folder, keep_mesh)
+
+    def forward(self, infer, data):
+        """Generate texture transfer samples per `batch_interval` batches.
+        """
+        if self.batch_total % self.batch_interval == 0:
+            p2p = corr2pointmap_vectorized(
+                corr_x=data['first']['corr'],
+                corr_y=data['second']['corr'],
+                num_verts_y=max(data['second']['num_verts']),
+            )
+
+            Pyx = pointmap2Pyx_vectorized(
+                p2p=p2p,
+                evecs_x=data['first']['evecs'],
+                evecs_y=data['second']['evecs'],
+                evecs_trans_x=data['first']['evecs_trans'],
+                evecs_trans_y=data['second']['evecs_trans'],
+                verts_mask_y=data['second']['verts_mask'],
             )
 
             name_x, name_y = data['first']['name'], data['second']['name']
--- src/utils/fmap.py
+++ src/utils/fmap.py
@@ -48,6 +48,36 @@ def pointmap2fmap(p2p, evecs_x, evecs_y):
     return C21
 
 
+def corr2pointmap_vectorized(corr_x, corr_y, num_verts_y):
+    """
+    Convert a pair of correspondences to point-to-point map in a vectorized manner.
+    Args:
+        corr_x (torch.Tensor): Correspondences from template to target. Shape [B, V_t] _P.S. V_t is the number of vertices in the template shape._
+        corr_y (torch.Tensor): Correspondences from target to template. Shape [B, V_t]
+        num_verts_y (int): Number of vertices in the target shape.
+    Returns:
+        p2p (torch.Tensor): Point-to-point map (shape y -> shape x). Shape [B, V_y] _P.S. Padded points will have value -1.
+    """
+    # template -(corr_x)-> shape x <--> shape y <-(corr_y)- target
+    # i.e. the i-th row of corr_y is correspongding with the i-th row of corr_x
+    B, V_t = corr_x.shape
+    batch_idx = torch.arange(B, device=corr_y.device).unsqueeze(1).expand(B, V_t)  # [B, V_t]
+    p2p_t = torch.full((B, V_t), -1, dtype=torch.long).to(device=corr_y.device)
+    p2p_t[batch_idx, corr_y] = corr_x
+
+    # get p2p in shape [B, V_y]
+    V_y = num_verts_y
+
+    if V_t > V_y:
+        p2p = p2p_t[:, :V_y]
+
+    else:
+        p2p = torch.full((B, V_y), -1, dtype=torch.long).to(device=corr_y.device)
+        p2p[:, :V_t] = p2p_t
+    
+    return p2p
+
+
 def fmap2pointmap_vectorized(Cxy, evecs_x, evecs_y, verts_mask_x, verts_mask_y):
     """
     Convert functional map to point-to-point map for a  (padded) batch of data in a vectorized manner
@@ -60,7 +90,7 @@ def fmap2pointmap_vectorized(Cxy, evecs_x, evecs_y, verts_mask_x, verts_mask_y):
         verts_mask_y (torch.Tensor): mask for vertices in shape y. Shape [B, V_y] with valid points as 1 and padded points as 0.
 
     Returns:
-        p2p (torch.Tensor): point-to-point map (shape y -> shape x). Shape [B, V_y]. _P.S. This function works for padded batched data, and the padded points will also have value in the returned `p2p` map. You should carefully masked them out, otherwise undefined behavior may be introduced._
+        p2p (torch.Tensor): point-to-point map (shape y -> shape x). Shape [B, V_y]. _P.S. Padded points will have value -1._
     """
     # compute point-to-point map from y to x using the fmap Cxy
     dist = torch.cdist(
@@ -75,10 +105,13 @@ def fmap2pointmap_vectorized(Cxy, evecs_x, evecs_y, verts_mask_x, verts_mask_y):
 
     # compute point-to-point map from y to x
     p2p = torch.argmin(dist, dim=-1) # [B, V_y]
-    
+
+    # set indices for masked y to -1
+    p2p = torch.where(verts_mask_y.bool(), p2p, -1)
+
     return p2p.long()
 
-def pointmap2Cxy_vectorized(p2p, evecs_x, evecs_trans_y):
+def pointmap2Cxy_vectorized(p2p, evecs_x, evecs_trans_y, verts_mask_y):
     """
     Convert point-to-point mapping to functional map for a  (padded) batch of data in a vectorized manner.
 
@@ -86,21 +119,22 @@ def pointmap2Cxy_vectorized(p2p, evecs_x, evecs_trans_y):
         p2p (torch.Tensor): Point-to-point mapping of shape y -> x. Shape [B, V_y].
         evecs_x (torch.Tensor): Eigenvectors of shape x. Shape [B, V_x, K].
         evecs_trans_y (torch.Tensor): Transposed eigenvectors of shape y. Shape [B, K, V_y].
+        verts_mask_y (torch.Tensor): mask for vertices in shape y. Shape [B, V_y] with valid points as 1 and padded points as 0.
 
     Returns:
         torch.Tensor: Functional map (shape x -> shape y). Shape [B, K, K].
     """
-    # expand p2p to (B, V, K) & gather along dim=1 (the V dimension)
+    # expand p2p to [B, V, K] & gather along dim=1 (the V dimension)
     # this will get Pyx @ Phi_x in effect
     K = evecs_x.shape[-1]
-    index = p2p.unsqueeze(-1).expand(-1, -1, K)
-    permuted_evecs_x = torch.gather(evecs_x, dim=1, index=index)
+    index = torch.clamp(p2p, min=0).unsqueeze(-1).expand(-1, -1, K) # [B, V, K]
+    permuted_evecs_x = torch.gather(evecs_x, dim=1, index=index) * verts_mask_y.unsqueeze(-1) # permute & mask out invalid rows
 
     # Cxy = Phi_y^T @ Pyx @ Phi_x
     return evecs_trans_y @ permuted_evecs_x
 
 
-def pointmap2Pyx_vectorized(p2p, evecs_x, evecs_y, evecs_trans_x, evecs_trans_y):
+def pointmap2Pyx_vectorized(p2p, evecs_x, evecs_y, evecs_trans_x, evecs_trans_y, verts_mask_y):
     """
     Convert point-to-point mapping to the permutation matrix.
 
@@ -110,11 +144,12 @@ def pointmap2Pyx_vectorized(p2p, evecs_x, evecs_y, evecs_trans_x, evecs_trans_y)
         evecs_y (torch.Tensor): Eigenvectors of shape y. Shape [B, V_y, K].
         evecs_trans_x (torch.Tensor): Transposed eigenvectors of shape x. Shape [B, K, V_x].
         evecs_trans_y (torch.Tensor): Transposed eigenvectors of shape y. Shape [B, K, V_y].
+        verts_mask_y (torch.Tensor): mask for vertices in shape y. Shape [B, V_y] with valid points as 1 and padded points as 0.
 
     Returns:
         torch.tensor: permutation matrix of shape [B, V_y, V_x].
     """
-    Cxy = pointmap2Cxy_vectorized(p2p, evecs_x, evecs_trans_y)
+    Cxy = pointmap2Cxy_vectorized(p2p, evecs_x, evecs_trans_y, verts_mask_y)  
     Pyx = evecs_y @ Cxy @ evecs_trans_x
 
     return Pyx
